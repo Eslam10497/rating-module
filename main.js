e17bfc3d0d477f2b64e48c7fc4a5ce1658df4b02
Eslam Wael <103150616+Eslam10497@users.noreply.github.com>
2022-06-16 02:48:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+///////////////Functions section//
+
+function getRating(rate, confirm) {
+  $(".sel").removeClass("highlight");
+  $("#" + rate).addClass("highlight");
+  switch (rate) {
+    case "one":
+      $("#text-rating").text("You selected 1 out of 5");
+      break;
+    case "two":
+      $("#text-rating").text("You selected 2 out of 5");
+
+      break;
+    case "three":
+      $("#text-rating").text("You selected 3 out of 5");
+
+      break;
+    case "four":
+      $("#text-rating").text("You selected 4 out of 5");
+
+      break;
+    case "five":
+      $("#text-rating").text("You selected 5 out of 5");
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+////Main Section/////
+
+$(".sel").click(function (e) {
+  const rating = $(this).attr("id");
+  getRating(rating);
+  if (confirm) {
+    $("#page-one").addClass(disappear);
+    $("#page-two").removeClass(disappear);
+  }
+});
+
+$("#submit-btn").click(function (e) {
+  e.preventDefault();
+  $("#page-one").addClass("disappear");
+  $("#page-two").removeClass("disappear");
+});
